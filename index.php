09f89e1885b6283a566679e2344998ce497663fe
Nathan Delavictoire <delavicn@tcd.ie>
2014-03-23 15:29:55 +0000

    Modification of the index with bootstrap css. The original code is in the content. It should be adapted to the buidled interface in the header and left columns. Original index.php in index_original.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 // some supporting files
@@ -10,65 +11,263 @@ $current = getCurrentAlgo();
 $algo_file = explode("\n", trim(file_get_contents(ALGO_FILE)));
 
 ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-<html>
-	<head>
-		<title>HAProxy_hotswap framework</title>
-		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-		<script>
-		$(function () {
-			$('#algo_switch_form').on('submit', function (e) {
-				$.ajax({
-					type: 'post',
-					url: 'swap_files/swap.php',
-					data: $('form').serialize(),
-					success: function (retval) {
-						if (retval=='error') {
-							document.getElementById("algo_status").outerHTML="<a id=\"algo_status\" style=\"color:red\">UNKNOWN ERROR</a>";
-						}
-						else if (retval=='ignore') {
-							// ignore return
-						}
-						else
-							document.getElementById("algo_status").outerHTML="<a id=\"algo_status\">current: "+retval+"</a>"
-					}
-				});
-				e.preventDefault();
-			});
-		});
-		</script>
-		
+    <title>Haproxy hotswap framework</title>
 
-	</head>
-	<body>
-		<center>
-			<div id="container">
-				<div id="algo_switcher" style="text-align:left; vertical-align:top; display:inline-block; padding:0 10px 0 10px">
-					<a>Algorithm switcher</a>
-					<form id="algo_switch_form">
-						<select name="LB_ALGO">
-						<?
-							foreach ($algo_file as $algo) {
-								echo "<option value=\"$algo\">$algo</option>";
-							}
-						?>
-						</select>
-						<input type="submit" class="submit" value="Submit" />
-						<br>
-						<a id="algo_status">current: <? echo $current ?></a>
-					</form>
-				</div>
-				
-				<div id="load_generator" style="text-align:left; vertical-align:top; display:inline-block; padding:0 10px 0 10px">
-					<a>Load generator</a>
-					<form method="POST" action="load_generator.php">
-						<input type="text" placeholder="duration (s)">
-						<br>
-						<input type="text" placeholder="amount (packets/s)">
-						<input type="submit" value="Generate">
-					</form>
-				</div>
-			</div>
-		</center>
-	</body>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+    $(function () {
+      $('#algo_switch_form').on('submit', function (e) {
+        $.ajax({
+          type: 'post',
+          url: 'swap_files/swap.php',
+          data: $('form').serialize(),
+          success: function (retval) {
+            if (retval=='error') {
+              document.getElementById("algo_status").outerHTML="<a id=\"algo_status\" style=\"color:red\">UNKNOWN ERROR</a>";
+            }
+            else if (retval=='ignore') {
+              // ignore return
+            }
+            else
+              document.getElementById("algo_status").outerHTML="<a id=\"algo_status\">current: "+retval+"</a>"
+          }
+        });
+        e.preventDefault();
+      });
+    });
+    </script>
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Haproxy hotswap framework</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><button type="button" class="btn btn-primary btn-lg active"><strong>Run</strong></button></li>
+            <li><a><strong>Delay :</strong></a></li>
+            <li><button type="button" class="btn btn-default btn-lg active"><strong> + </strong></button></li>
+            <li><button type="button" class="btn btn-default btn-lg active"><strong> - </strong></button></li>
+            <li><a><strong>80 secondes</strong></a></li>
+            <li><a href="#"><strong>Help</strong></a></li>
+          </ul>
+          <!--<form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>-->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">swap_mode_1</a></li>
+            <li ><a href="#">swap_mode_2</a></li>
+            <li ><a href="#">swap_mode_3</a></li>
+            <li ><a href="#">swap_mode_4</a></li>
+          </ul>
+        </div>
+
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Results : </h1>
+          <div id="container">
+        <div id="algo_switcher" style="text-align:left; vertical-align:top; display:inline-block; padding:0 10px 0 10px">
+          <a>Algorithm switcher</a>
+          <form id="algo_switch_form">
+            <select name="LB_ALGO">
+            <?
+              foreach ($algo_file as $algo) {
+                echo "<option value=\"$algo\">$algo</option>";
+              }
+            ?>
+            </select>
+            <input type="submit" class="submit" value="Submit" />
+            <br>
+            <a id="algo_status">current: <? echo $current ?></a>
+          </form>
+        </div>
+        
+        <div id="load_generator" style="text-align:left; vertical-align:top; display:inline-block; padding:0 10px 0 10px">
+          <a>Load generator</a>
+          <form method="POST" action="load_generator.php">
+            <input type="text" placeholder="duration (s)">
+            <br>
+            <input type="text" placeholder="amount (packets/s)">
+            <input type="submit" value="Generate">
+          </form>
+        </div>
+      </div>
+
+          <!--<h2 class="sub-header">Section title</h2>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>Lorem</td>
+                  <td>ipsum</td>
+                  <td>dolor</td>
+                  <td>sit</td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+                  <td>elit</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                  <td>Praesent</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+                  <td>ante</td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+                  <td>nisi</td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>Nulla</td>
+                  <td>quis</td>
+                  <td>sem</td>
+                  <td>at</td>
+                </tr>
+                <tr>
+                  <td>1,006</td>
+                  <td>nibh</td>
+                  <td>elementum</td>
+                  <td>imperdiet</td>
+                  <td>Duis</td>
+                </tr>
+                <tr>
+                  <td>1,007</td>
+                  <td>sagittis</td>
+                  <td>ipsum</td>
+                  <td>Praesent</td>
+                  <td>mauris</td>
+                </tr>
+                <tr>
+                  <td>1,008</td>
+                  <td>Fusce</td>
+                  <td>nec</td>
+                  <td>tellus</td>
+                  <td>sed</td>
+                </tr>
+                <tr>
+                  <td>1,009</td>
+                  <td>augue</td>
+                  <td>semper</td>
+                  <td>porta</td>
+                  <td>Mauris</td>
+                </tr>
+                <tr>
+                  <td>1,010</td>
+                  <td>massa</td>
+                  <td>Vestibulum</td>
+                  <td>lacinia</td>
+                  <td>arcu</td>
+                </tr>
+                <tr>
+                  <td>1,011</td>
+                  <td>eget</td>
+                  <td>nulla</td>
+                  <td>Class</td>
+                  <td>aptent</td>
+                </tr>
+                <tr>
+                  <td>1,012</td>
+                  <td>taciti</td>
+                  <td>sociosqu</td>
+                  <td>ad</td>
+                  <td>litora</td>
+                </tr>
+                <tr>
+                  <td>1,013</td>
+                  <td>torquent</td>
+                  <td>per</td>
+                  <td>conubia</td>
+                  <td>nostra</td>
+                </tr>
+                <tr>
+                  <td>1,014</td>
+                  <td>per</td>
+                  <td>inceptos</td>
+                  <td>himenaeos</td>
+                  <td>Curabitur</td>
+                </tr>
+                <tr>
+                  <td>1,015</td>
+                  <td>sodales</td>
+                  <td>ligula</td>
+                  <td>in</td>
+                  <td>libero</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>-->
+        </div>
+      </div>
+    </div>
+    
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+  </body>
 </html>
