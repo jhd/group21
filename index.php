aa32bab8a19b7d23704979181b113f0ceed0507c
Nathan Delavictoire <delavicn@tcd.ie>
2014-03-23 15:45:26 +0000

    Little modification of index.php

--- index.php
+++ index.php
@@ -77,9 +77,9 @@ $algo_file = explode("\n", trim(file_get_contents(ALGO_FILE)));
           <ul class="nav navbar-nav navbar-right">
             <li><button type="button" class="btn btn-primary btn-lg active"><strong>Run</strong></button></li>
             <li><a><strong>Delay :</strong></a></li>
+            <li><a><strong>80 sec</strong></a></li>
             <li><button type="button" class="btn btn-default btn-lg active"><strong> + </strong></button></li>
             <li><button type="button" class="btn btn-default btn-lg active"><strong> - </strong></button></li>
-            <li><a><strong>80 secondes</strong></a></li>
             <li><a href="#"><strong>Help</strong></a></li>
           </ul>
           <!--<form class="navbar-form navbar-right">
